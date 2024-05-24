d0ad6a264e14f756c6b111c05cfc2ebf6ba26dff
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-05-24 16:04:32 +0530

    project done counter app

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,19 +5,30 @@ import './App.css'
 
 function App() {
 
-  const [counter, setCounter] = useState(15)
+  const [counter, setCounter] = useState(0)
+  //  let counter = 5 
 
-let addValue = () => {
-  console.log("clicked",Math.random);
-  counter = counter + 1;
+  const addValue = () => {
+    
+    // counter = counter + 1
+    setCounter(counter + 1)
+    console.log("Clicked", counter);
+  }
+
+
+const removeValue = () => {
+  
+  setCounter(counter - 1)
+  console.log("Clicked", counter);
 }
+
 
   return (
     <>
       <h1>Hi Pradeep</h1>
-      <h2>counter Value : {counter}</h2>
-      <button onClick={addValue}>Increase</button>
-      <button>Decrease</button>
+      <h2>counter Value : {counter} </h2>
+      <button onClick={addValue}>Increase {counter}</button>
+      <button onClick={removeValue}>Decrease</button>
     </>
   )
 }
