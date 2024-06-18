79d9a7cc259e50e965abfe0acf809a0aa83b1c9d
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-06-18 16:24:29 +0530

    add API

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1 className='text-3xl bg-orange-600'>Currency Converter</h1>
-    </>
-  )
+    <div>
+      <h1  className="text-4xl bg-teal-400 text-center mt-8">Currency Converter</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
