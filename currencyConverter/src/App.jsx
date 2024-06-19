009b449c6d9f6b500258a16eda138d8c403e73a2
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-06-19 20:48:42 +0530

    add text

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1  className="text-4xl bg-teal-400 text-center mt-8">Currency Converter</h1>
+      <h1  className="text-4xl bg-teal-400 text-center mt-8">Currency Converter App</h1>
     </div>
   );
 }
