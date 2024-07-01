d7ac152918ca254fbe8ed98ce329b900b34c1598
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-07-01 12:30:41 +0530

    add Home page to the project

---
+++ 07ReactRouter/src/Layout.jsx
@@ -0,0 +1,15 @@
+
+import Header from "./componentes/Header/Header"
+import Footer from "./componentes/Footer/Footer"
+import { Outlet } from "react-router-dom"
+
+export default  function Layout(){
+
+    return(
+        <>
+       <Header />
+        <Outlet />
+       <Footer />
+        </>
+    )
+}
--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import About from './componentes/About/About'
+import Layout from './Layout'
+import Home from "./componentes/Home/Home"
 
-const router = createBrowserRouter([])
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
