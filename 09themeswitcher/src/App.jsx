dd7c743371b7e5030429b3dfbdfb365b0ac8e7e2
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-07-15 22:47:16 +0530

    add provider

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,11 +1,28 @@
 
+import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/theme'
+import { useEffect } from 'react'
 
 function App() {
+const [themeMode, setThemeMode] = useState("light")
 
+const lightMode = () => {
+  setThemeMode("light")
+}
+
+
+const darkMode = () => {
+  setThemeMode("dark")
+}
+
+useEffect(() => {
+  document.querySelector('html').classList.remove('ligh' , 'dark')
+  document.querySelector('html').classList.remove(themeMode)
+}, [themeMode]) 
 
   return (
-   
+   <ThemeProvider value={{ lightMode, themeMode ,darkMode}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -17,6 +34,7 @@ function App() {
                     </div>
                 </div>
             </div>
+      </ThemeProvider>
 
   )
 }
