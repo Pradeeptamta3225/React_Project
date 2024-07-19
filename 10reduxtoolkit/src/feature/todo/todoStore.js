87169e17d19f9664de491d78807777dc2568f58b
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-07-19 17:35:06 +0530

    create reducer

--- 10reduxtoolkit/src/feature/todo/todoStore.js
+++ 10reduxtoolkit/src/feature/todo/todoStore.js
@@ -1,25 +1,73 @@
-import {createSlice, nanoid} from '@reduxjs/toolkit'
+import { createReducer, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id: 1, name: "Pradeep"}]
+    todos: {id:1,name: 'Pradeep'}
 }
 
-
-export const todoSlice = createSlice({
-    name: 'todo',
+export const todoStore = createReducer({
+    id: 'todo',
     initialState,
-    reducers: {
-      addTodo: (state, action) => {
-        const todo = {
-            id: nanoid(),
-            text: action.payload
-        }
+    reducer:{
+        addTodo: (state,action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
             state.todos.push(todo)
-      },
-     
-     removeTodo: (state, action) => {
-        state.todos = state.todos.filter((todo) => todo.id !==action.payload)
-     }
-
+        },
+        removeTodo: (state, action) => {
+            state.todo = state.todo.filter((todo) => todo.id !== action.payload)
+        }
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {createSlice, nanoid} from '@reduxjs/toolkit'
+
+// const initialState = {
+//     todos: [{id: 1, name: "Pradeep"}]
+// }
+
+
+// export const todoSlice = createSlice({
+//     name: 'todo',
+//     initialState,
+//     reducers: {
+//       addTodo: (state, action) => {
+//         const todo = {
+//             id: nanoid(),
+//             text: action.payload
+//         }
+//             state.todos.push(todo)
+//       },
+     
+//      removeTodo: (state, action) => {
+//         state.todos = state.todos.filter((todo) => todo.id !==action.payload)
+//      }
+
+//     }
+// })
