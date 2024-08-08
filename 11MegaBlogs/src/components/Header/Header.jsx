0f1d727d27b203351c8c6b873acbfbe053e09d52
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-08-08 16:27:23 +0530

    add pages and developed authLayout

--- 11MegaBlogs/src/components/Header/Header.jsx
+++ 11MegaBlogs/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import {Container, Logo, LogoutBtn} from '../Header/index'
+import React from 'react'
+import {Container, Logo, LogoutBtn} from '../index'
 import { Link } from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
