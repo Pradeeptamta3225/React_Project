e504cbe9052c5faacb6ef49d8bcd8861e0666acb
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-08-13 22:13:32 +0530

    add pages

--- 11MegaBlogs/src/main.jsx
+++ 11MegaBlogs/src/main.jsx
@@ -5,17 +5,17 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './components/pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 
 
-import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
-import EditPost from "./pages/EditPost";
+import AddPost from "./components/pages/AddPost";
+import Signup from './components/Signup'
+import EditPost from "./components/pages/EditPost";
 
-import Post from "./pages/Post";
+import Post from "./components/pages/Post";
 
-import AllPosts from "./pages/AllPosts";
+import AllPosts from "./components/pages/AllPost.jsx";
 
 const router = createBrowserRouter([
   {
